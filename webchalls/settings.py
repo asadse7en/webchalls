d4e4ca44e24dfb958a654e6c841fa405e81c0007
Asad Ullah <100380926+asadse7en@users.noreply.github.com>
2023-05-15 02:21:06 +0500

    commit

--- webchalls/settings.py
+++ webchalls/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#&b^mg(%+3_t#pt^z+jir%(a^a=-8_cmz060!39w4(@nt#v^f6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'webchalls.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'webchalls.herokuapp.com', 'challs.aupctf.live']
 
 
 # Application definition
