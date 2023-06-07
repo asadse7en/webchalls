d737f3c2f47046da8ae7b51e6f4688e2d384ea06
Asad Ullah <100380926+asadse7en@users.noreply.github.com>
2023-06-08 02:18:52 +0500

    commit

--- conundrum/urls.py
+++ conundrum/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 
 from . import views
@@ -9,6 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('username/', views.user_view, name='usernames'),
-    path('password/', views.password_view, name='passwords'),
+    path('usernames/', views.user_view, name='usernames'),
+    path('passwords/', views.password_view, name='passwords'),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+
 ]
--- webchalls/settings.py
+++ webchalls/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#&b^mg(%+3_t#pt^z+jir%(a^a=-8_cmz060!39w4(@nt#v^f6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'webchalls.herokuapp.com', 'challs.aupctf.live']
 
