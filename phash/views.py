1b57fc3c5b72d02308fa00127da08c2706f4bbef
Asad Ullah <100380926+asadse7en@users.noreply.github.com>
2023-06-23 03:32:10 +0500

    commitcommit

--- phash/views.py
+++ phash/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def login(request):
 
-    random_word = 'ghostrider'
+    random_word = 'mephisto'
     random_md5 = hashlib.md5(random_word.encode('utf-8')).hexdigest()
 
     if request.method == 'POST':
@@ -15,7 +15,7 @@ def login(request):
         password = request.POST.get('password')
 
         if username == 'admin' and password == random_md5:
-            messages.success(request, 'Congratulations! here is your flag: aupCTF{y0u-ar3-a-ha5hcr4ck1ng-m4ch1n3}')
+            messages.success(request, 'Congratulations! here is your flag: aupCTF{y0u-ar3-a-tru3-m4rv3l-f4n}')
         else:
             messages.error(request, 'Invalid username or password.')
 
